b09e74f4cfce54ba027004bc8e9e6ff0775794c9
XiongMinqi <1447259297@qq.com>
2019-12-11 14:34:48 +0800

    finish

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,293 @@
-//index.js
-//获取应用实例
 const app = getApp()
-
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    indicatorDots: true,
+    vertical: false,
+    interval: 10,
+    duration: 500,
+    value: "",
+    banner: [],
+    id: 0,
+    pid: 0,
+    words: false,//当搜索有结果时显示
+    num: 1,
+    histories: [],
+    history: [],
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+  //点击热搜搜索
+  searchWord(e){
+    console.log(e)
+    this.setData({
+      value:e.currentTarget.dataset.value,
+      words:true
+    })
+    this.searches()
+  },
+  // 点击历史搜索
+  historylist(e){
+    console.log(e)
+    this.setData({
+      value: e.currentTarget.dataset.item,
+      words: true
+    })
+    this.searches()
+  },
+  //清空搜索历史
+  clear(){
+    wx.removeStorageSync("history")
+    let histories = wx.getStorageSync("history")
+    this.setData({
+      histories
     })
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
+  // 热搜
+  hot(){
+    wx.showLoading({
+      title: '加载中...',
+    })
+    app.globalData.fly.get(`/search/hot/detail`).then(res => {
+      wx.hideLoading()
+      let hotlist = res.data.data
       this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
+        hotlist
       })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+      // console.log(res)
+      // console.log(this.data.hotlist, "hotlist")
+    }).catch(err => {
+      console.log(err)
+      wx.hideLoading()
+    })
+  },
+  // 歌手详情
+  musician(e) {
+    console.log(e)
+    let input = e.currentTarget.dataset.item
+    let histories = wx.getStorageSync("history")
+    this.setData({
+      input,
+      histories,
+      history: histories
+    })
+    console.log(this.data.histories)
+    // console.log(this.data.value)
+    console.log(this.data.input)
+    if (this.data.histories.length === 0) {
+      console.log("本地没数据")
+      this.data.history = []
+      this.data.history.push(this.data.value)
+      wx.setStorageSync("history", this.data.history)
+    } else if (this.data.histories && this.data.histories.length < 6 && this.data.value !== "") {
+      console.log("小于六")
+      if (this.data.histories.indexOf(this.data.value) === -1) {
+        this.data.history.unshift(this.data.value);
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
+      wx.setStorageSync("history", this.data.history)
+      console.log(this.data.history)
+    } else if (this.data.histories && this.data.histories.length >= 6 && this.data.value !== "") {
+      console.log("大于六")
+      if (this.data.histories.indexOf(this.data.value) === -1) {
+        this.data.history.pop();
+        this.data.history.unshift(this.data.value);
+      }
+      wx.setStorageSync("history", this.data.history)
+    }
+    wx.navigateTo({
+      url: `/pages/search/search?value=${this.data.input}`,
+    })
+  },
+  // 歌曲播放
+  musicdetail(e) {
+    console.log(e)
+    let inputitem = e.currentTarget.dataset.item
+    let histories = wx.getStorageSync("history")
+    this.setData({
+      inputitem,
+      histories,
+      history: histories
+    })
+    console.log(this.data.value)
+    if (this.data.histories.length === 0) {
+      // console.log("本地没数据")
+      this.data.history = []
+      this.data.history.push(this.data.value)
+      wx.setStorageSync("history", this.data.history)
+    } else if (this.data.histories && this.data.histories.length < 6 && this.data.value !== "") {
+      // console.log("小于六")
+      if (this.data.histories.indexOf(this.data.value) === -1) {
+        this.data.history.unshift(this.data.value);
+      }
+      wx.setStorageSync("history", this.data.history)
+      console.log(this.data.history)
+    } else if (this.data.histories && this.data.histories.length >= 6 && this.data.value !== "") {
+      // console.log("大于六")
+      if (this.data.histories.indexOf(this.data.value) === -1) {
+        this.data.history.pop();
+        this.data.history.unshift(this.data.value);
+      }
+      wx.setStorageSync("history", this.data.history)
+    }
+     wx.navigateTo({
+       url: `/pages/search/search?value=${this.data.inputitem}`,
+    })
+  },
+  onCancel() {
+    this.setData({
+      num: 1
+    })
+  },
+  onsearch(){
+    this.setData({
+      num: 0
+    })
+  },
+  oninput(e) {
+    console.log(e)
+    this.setData({
+      value: e.detail,
+      num: 0
+    })
+  },
+  // 搜索共同方法
+  searches(){
+    if (this.data.value === '') {
+      let histories = wx.getStorageSync("history")
+      this.setData({
+        words: false,
+        histories
+      })
+      // console.log(this.data.histories)
+    } else if (this.data.value !== '') {
+      wx.showLoading({
+        title: '搜索中...',
+      })
+      app.globalData.fly.get(`/search/suggest?keywords= ${this.data.value}`).then(res => {
+        wx.hideLoading()
+        let result = res.data.result
+        this.setData({
+          result
+        })
+        console.log(res)
+        console.log(this.data.result, "result")
+      }).catch(err => {
+        console.log(err)
+        wx.hideLoading()
       })
     }
   },
-  getUserInfo: function(e) {
+  // 搜索
+  onSearch(e) {
+    // console.log(e)
+      this.setData({
+        value: e.detail,
+        words: true
+      })
+    this.searches()
+  },
+  searchvalue(e){
     console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+    let value = e.currentTarget.dataset.item
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      value
     })
+    wx.navigateTo({
+      url: `/pages/search/search?value=${this.data.value}`,
+    })
+  },
+  //轮播图
+  getShuffling() {
+    wx.showLoading({
+      title: '加载中...',
+    })
+    app.globalData.fly.get(`/banner`).then(res => {
+      wx.hideLoading()
+      let banner = res.data.banners
+      this.setData({
+        banner
+      })
+      // console.log(res)
+      // console.log(this.data.banner, "banner")
+    }).catch(err => {
+      console.log(err)
+      wx.hideLoading()
+    })
+  },
+  playvedio(e) {
+    console.log(e)
+    let id = e.currentTarget.dataset.item.targetId
+    let pid = e.currentTarget.dataset.item.encodeId
+    this.setData({
+      id,
+      pid
+    })
+    wx.navigateTo({
+      url: `/pages/playing/playing?playid=${this.data.id}&indexflag=true`,
+    })
+    console.log(this.data.id)
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    this.getShuffling()
+    this.hot()
+    let histories = wx.getStorageSync("history")
+    this.setData({
+      histories
+    })
+    // console.log(this.data.histories)
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
   }
 })
